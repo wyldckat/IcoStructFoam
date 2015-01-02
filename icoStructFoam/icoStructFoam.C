eaebaa7cb579084e6375eba0019fd2735de685e8
wyldckat <wyldckat@gmail.com>
2015-01-02 20:42:56 +0000

    Updated the source code for 'icoStructFoam.C', based on the change made in OpenFOAM 2.2 to 2.3 in pisoFoam.

--- icoStructFoam/icoStructFoam.C
+++ icoStructFoam/icoStructFoam.C
@@ -80,23 +80,26 @@ int main(int argc, char *argv[])
     {
         Info<< "Time = " << runTime.timeName() << nl << endl;
 
-	if(runTime.value()>=startMeshMotion.value()) {
-	  Info << "\nMoving mesh\n" << endl;
+        if(runTime.value()>=startMeshMotion.value())
+        {
+          Info << "\nMoving mesh\n" << endl;
 
           // Make the fluxes absolute
           //          fvc::makeAbsolute(phi, U);
 
-          //	  dispVals=inter.faceToPointInterpolate(displace);
+          //          dispVals=inter.faceToPointInterpolate(displace);
           dispVals=displace;
 
-	  scalar maxDist=-1e10;
-	  scalar maxAway=-1e10;
+          scalar maxDist=-1e10;
+          scalar maxAway=-1e10;
 
-          if(displacementMotionSolver) {
+          if(displacementMotionSolver)
+          {
               volVectorField::GeometricBoundaryField &meshDisplacement=
                   const_cast<volVectorField&>(mesh1.objectRegistry::lookupObject<volVectorField>("cellDisplacement")).boundaryField();
 
-              if(typeid(meshDisplacement[fluidSideI])!=typeid(fixedValueFvPatchField<vector>)) {
+              if(typeid(meshDisplacement[fluidSideI])!=typeid(fixedValueFvPatchField<vector>))
+              {
                   FatalErrorIn("Coupled Solver ") << "Fluid side not movable" << exit(FatalError);
               }
               
@@ -104,10 +107,11 @@ int main(int argc, char *argv[])
               
               scalar factor=1/motionRelaxation.value();
 
-              forAll(fluidMesh,fluidI) {
+              forAll(fluidMesh,fluidI)
+              {
                   label solidI=exchange[fluidI];
 
-                  //	    vector here=mesh1.points()[fluidPoints[fluidI]];
+                  //            vector here=mesh1.points()[fluidPoints[fluidI]];
                   vector here=fluidMesh.faceCentres()[fluidI];
                   vector old =oldPoints[fluidI];
                   vector disp=dispVals[solidI];
@@ -116,34 +120,40 @@ int main(int argc, char *argv[])
 
                   mDisp[fluidI]=move;
 
-                  if(mag(here-old)>maxDist) {
+                  if(mag(here-old)>maxDist)
+                  {
                       maxDist=mag(here-old);
                   }
-                  if(mag(neu-here)>maxAway) {
+                  if(mag(neu-here)>maxAway)
+                  {
                       maxAway=mag(here-neu);
                   }
               }
 
               
-          } else {
+          }
+          else
+          {
               volVectorField::GeometricBoundaryField &motionU=
                   const_cast<volVectorField&>(mesh1.objectRegistry::lookupObject<volVectorField>("cellMotionU")).boundaryField();
               
-              if(typeid(motionU[fluidSideI])!=typeid(fixedValueFvPatchField<vector>)) {
+              if(typeid(motionU[fluidSideI])!=typeid(fixedValueFvPatchField<vector>))
+              {
                   FatalErrorIn("Coupled Solver ") << "Fluid side not movable" << exit(FatalError);
               }
               
               vectorField &patchU=refCast<vectorField>(motionU[fluidSideI]);
 
-              //	  tetPointVectorField neu=inter.faceToPointInterpolate(patchU);
+              //          tetPointVectorField neu=inter.faceToPointInterpolate(patchU);
               scalar factor=1/(runTime.deltaT().value()*motionRelaxation.value());
               //          const labelList& fluidPoints=fluidPatch.meshPoints();
 
-              //	  forAll(fluidPoints,fluidI) {
-              forAll(fluidMesh,fluidI) {
+              //          forAll(fluidPoints,fluidI) {
+              forAll(fluidMesh,fluidI)
+              {
                   label solidI=exchange[fluidI];
 
-                  //	    vector here=mesh1.points()[fluidPoints[fluidI]];
+                  //            vector here=mesh1.points()[fluidPoints[fluidI]];
                   vector here=fluidMesh.faceCentres()[fluidI];
                   vector old =oldPoints[fluidI];
                   vector disp=dispVals[solidI];
@@ -152,116 +162,119 @@ int main(int argc, char *argv[])
 
                   patchU[fluidI]=move;
 
-                  if(mag(here-old)>maxDist) {
+                  if(mag(here-old)>maxDist)
+                  {
                       maxDist=mag(here-old);
                   }
-                  if(mag(neu-here)>maxAway) {
+                  if(mag(neu-here)>maxAway)
+                  {
                       maxAway=mag(here-neu);
                   }
               }
           }
-	  mesh1.movePoints(motionPtr->newPoints());
+          mesh1.movePoints(motionPtr->newPoints());
           //          U.correctBoundaryConditions();
 
-	  Info << "\nBiggest movement: " << maxDist << " Bigges divergence " << maxAway <<  endl;
+          Info << "\nBiggest movement: " << maxDist << " Bigges divergence " << maxAway <<  endl;
 
           // Make the fluxes relative
           //          fvc::makeRelative(phi, U);
-	}
+        }
 
-	Info << "Solving flow in mesh1\n" << endl;
-	{
+        Info << "Solving flow in mesh1\n" << endl;
+        {
 #         include "readPISOControls.H"
 #         include "CourantNo.H"
 
-	  fvVectorMatrix UEqn
-	    (
-	     fvm::ddt(U)
-	     + fvm::div(phi, U)
-	     - fvm::laplacian(nu, U)
-	     );
+          fvVectorMatrix UEqn
+            (
+             fvm::ddt(U)
+             + fvm::div(phi, U)
+             - fvm::laplacian(nu, U)
+             );
 
-	  solve(UEqn == -fvc::grad(p));
+          solve(UEqn == -fvc::grad(p));
 
-	  // --- PISO loop
+          // --- PISO loop
 
-	  for (int corr=0; corr<nCorr; corr++)
-	    {
-	      volScalarField rUA = 1.0/UEqn.A();
+          for (int corr=0; corr<nCorr; corr++)
+          {
+              volScalarField rUA = 1.0/UEqn.A();
 
-	      U = rUA*UEqn.H();
-	      phi = (fvc::interpolate(U) & mesh1.Sf()) 
-                + fvc::ddtPhiCorr(rUA, U, phi);
+              U = rUA*UEqn.H();
+              phi = (fvc::interpolate(U) & mesh1.Sf()) 
+                + fvc::interpolate(rUA)*fvc::ddtCorr(U, phi);
 
-	      adjustPhi(phi, U, p);
+              adjustPhi(phi, U, p);
 
-	      for (int nonOrth=0; nonOrth<=nNonOrthCorr; nonOrth++)
-		{
-		  fvScalarMatrix pEqn
-		    (
-		     fvm::laplacian(rUA, p) == fvc::div(phi)
-		     );
+              for (int nonOrth=0; nonOrth<=nNonOrthCorr; nonOrth++)
+              {
+                  fvScalarMatrix pEqn
+                    (
+                     fvm::laplacian(rUA, p) == fvc::div(phi)
+                     );
 
-		  pEqn.setReference(pRefCell, pRefValue);
-		  pEqn.solve();
+                  pEqn.setReference(pRefCell, pRefValue);
+                  pEqn.solve();
 
-		  if (nonOrth == nNonOrthCorr)
-		    {
-		      phi -= pEqn.flux();
-		    }
-		}
+                  if (nonOrth == nNonOrthCorr)
+                    {
+                      phi -= pEqn.flux();
+                    }
+              }
 
 #             include "continuityErrs.H"
 
-	      U -= rUA*fvc::grad(p);
-	      U.correctBoundaryConditions();
-	    }
+              U -= rUA*fvc::grad(p);
+              U.correctBoundaryConditions();
+          }
 
-	}
+        }
 
-	Info << "\nSolving structure in mesh2\n" << endl;
+        Info << "\nSolving structure in mesh2\n" << endl;
 
-	{
+        {
 #         include "readStressedFoamControls.H"
-	  int iCorr = 0;
-	  scalar initialResidual = 0;
+          int iCorr = 0;
+          scalar initialResidual = 0;
 
-	  do
-	    {
-                if (thermalStress)
-		{
-                    volScalarField& T = Tptr();
-                    solve
-                        (
-                            fvm::ddt(T) == fvm::laplacian(DT, T)
-                        );
-		}
+          do
+          {
+              if (thermalStress)
+              {
+                  volScalarField& T = Tptr();
+                  solve
+                      (
+                          fvm::ddt(T) == fvm::laplacian(DT, T)
+                      );
+              }
 
-                {
-                    fvVectorMatrix DEqn
-                        (
-                            fvm::d2dt2(D)
-                            ==
-                            fvm::laplacian(2*mu + lambda, D, "laplacian(DD,D)")
-                            + divSigmaExp
-                        );
+              {
+                  fvVectorMatrix DEqn
+                      (
+                          fvm::d2dt2(D)
+                          ==
+                          fvm::laplacian(2*mu + lambda, D, "laplacian(DD,D)")
+                          + divSigmaExp
+                      );
 
-                    if (thermalStress)
-                    {
-                        const volScalarField& T = Tptr();
-                        DEqn += threeKalpha*fvc::grad(T);
-                    }
+                  if (thermalStress)
+                  {
+                      const volScalarField& T = Tptr();
+                      DEqn += threeKalpha*fvc::grad(T);
+                  }
 
-                    //UEqn.setComponentReference(1, 0, vector::X, 0);
-                    //UEqn.setComponentReference(1, 0, vector::Z, 0);
+                  //UEqn.setComponentReference(1, 0, vector::X, 0);
+                  //UEqn.setComponentReference(1, 0, vector::Z, 0);
 
-                    initialResidual = DEqn.solve().initialResidual();
+                  initialResidual = DEqn.solve().initialResidual();
 
-                    if (!compactNormalStress)
-                    {
-                        divSigmaExp = fvc::div(DEqn.flux());
-                    }
-                }
+                  if (!compactNormalStress)
+                  {
+                      divSigmaExp = fvc::div(DEqn.flux());
+                  }
+              }
+                
               {
                   volTensorField gradD = fvc::grad(D);
                   sigmaD = mu*twoSymm(gradD) + (lambda*I)*tr(gradD);
@@ -277,24 +290,25 @@ int main(int argc, char *argv[])
                   }
               }
               
-	    } while (initialResidual > convergenceTolerance && ++iCorr < nCorr);
+          } while (initialResidual > convergenceTolerance && ++iCorr < nCorr);
 
 #       include "calculateStress.H"
  
-	  Info << "\nMaximum Displacement: " << max(mag(D)).value() << endl;
+          Info << "\nMaximum Displacement: " << max(mag(D)).value() << endl;
 
-	  
-	} 
+          
+        } 
 
-	Info << "\nCoupling the solutions\n" << endl;
+        Info << "\nCoupling the solutions\n" << endl;
 
-	scalarField & fluidP = p.boundaryField()[fluidSideI];
-	
- 	scalarField &solidP = displace.pressure();
-	
-	forAll(fluidP,fI) {
-	  solidP[exchange[fI]]=-fluidP[fI];
-	}
+        scalarField & fluidP = p.boundaryField()[fluidSideI];
+        
+         scalarField &solidP = displace.pressure();
+        
+        forAll(fluidP,fI)
+        {
+          solidP[exchange[fI]]=-fluidP[fI];
+        }
 
 #       include "write.H"
     }
